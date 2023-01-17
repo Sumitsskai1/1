3e4254baf135ed99d96b1056bc7eb57040d3eea9
kai1fuchu <yuuto_takahashi@smtss.local>
2023-01-17 09:19:03 +0900

    Initial Commit

---
+++ Shared/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  Shared
+//
+//  Created by SuMiTS&S 開発第一部 on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
